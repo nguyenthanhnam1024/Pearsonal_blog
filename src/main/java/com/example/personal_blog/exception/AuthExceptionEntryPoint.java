289c913298f9b22c91bafaad1f6ef77428e1ab1f
Nguyễn Thành Nam <ayevooi230222@gmail.com>
2023-08-21 16:15:21 +0700

    create AuthExceptionEntryPoint, WebSecurityConfig classes, configure cors, create data basic in PersonalBlogApplication.java

---
+++ src/main/java/com/example/personal_blog/exception/AuthExceptionEntryPoint.java
@@ -0,0 +1,21 @@
+package com.example.personal_blog.exception;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthExceptionEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        if (e.getMessage().equals("Expiration jwt")) {
+            httpServletResponse.sendError(400, e.getMessage());
+        }
+        httpServletResponse.sendError(httpServletResponse.getStatus(), e.getMessage());
+    }
+}
